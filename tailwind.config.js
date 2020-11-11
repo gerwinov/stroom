c9cf74d1cadc003cc957b5b3e7db5df0632c3d5d
Gerwin Overeem <gerwin@overeem.io>
2020-11-11 17:33:59 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["./public/**/*.html"],
+  theme: {
+    colors: {
+      pink: '#F9D9F9',
+      orange: '#FC9B59',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
