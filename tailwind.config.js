9f852973969ceab97dadd8d8aa8a09409b1c6f20
Gerwin Overeem <gerwin@overeem.io>
2020-11-11 20:18:58 +0100

    newsletter box

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@ module.exports = {
   purge: ["./public/**/*.html"],
   theme: {
     colors: {
+      white: '#FFF',
       pink: '#F9D9F9',
       orange: '#FC9B59',
       red: '#FF1300',
+      blue: '#0000FF',
+      turquoise: '#00FFFF',
+      magenta: '#FF00FF',
+      coal: '#282828',
     },
     extend: {
       inset: {
