dce32bf515553b29d22ae3fc8a95f5d42d15a0dc
Gerwin Overeem <gerwin@overeem.io>
2020-12-15 20:00:09 +0100

    Robots.txt generator added

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,17 @@
 const { merge }              = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const RobotstxtPlugin = require("robotstxt-webpack-plugin");
 
 const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
     plugins: [
         new CleanWebpackPlugin(),
+        new RobotstxtPlugin({
+            policy: [{
+                userAgent: "*",
+                disallow: "/",
+            }]
+        }),
     ],
 });
